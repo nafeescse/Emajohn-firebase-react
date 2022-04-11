0fe89c8d9bc680e92988e1d5da3577c5a66f1b21
Nafees <abdur17@student.sust.edu>
2022-04-12 05:22:33 +0600

    added

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-ivtc8yZMiXOAkqLr3afotjJ2crX0ZTQ",
+  authDomain: "ema-john-firebase-52575.firebaseapp.com",
+  projectId: "ema-john-firebase-52575",
+  storageBucket: "ema-john-firebase-52575.appspot.com",
+  messagingSenderId: "221554177147",
+  appId: "1:221554177147:web:d98b1789735bfed817dede",
+  measurementId: "G-6Y03BCTR6N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
